374230035565b33b89b63fe7a551d178f200a1ad
Miclell <hem123v@gmail.com>
2024-09-28 01:38:12 +0300

    add sqllite

--- Program.cs
+++ Program.cs
@@ -1,71 +1,63 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 using WriteOffDebts.Data;
-using WriteOffDebts.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using System.IdentityModel.Tokens.Jwt;
-using System.Net;
-using Microsoft.Extensions.Options;
 
-namespace WriteOffDebts
+namespace WriteOffDebts;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+        builder.Services.AddAuthentication(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            options.DefaultAuthenticateScheme = "Cookies";
+            options.DefaultSignInScheme = "Cookies";
+            options.DefaultChallengeScheme = "Cookies";
+        }).AddCookie(options =>
+        {
+            options.LoginPath = "/User/Login";
+            options.LogoutPath = "/User/Logout";
+            options.AccessDeniedPath = "/User/AccessDenied";
+        });
 
-            builder.Services.AddControllersWithViews();
-
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<AppDbContext>()
-                .AddDefaultTokenProviders();
-
-            builder.Services.AddAuthentication(options =>
+        builder.Services.AddControllersWithViews()
+            .AddViewOptions(options =>
             {
-                options.DefaultAuthenticateScheme = "Cookies";
-                options.DefaultSignInScheme = "Cookies";
-                options.DefaultChallengeScheme = "Cookies";
-            }).AddCookie(options =>
-            {
-                options.LoginPath = "/User/Login";
-                options.LogoutPath = "/User/Logout";
-                options.AccessDeniedPath = "/User/AccessDenied";
+                options.HtmlHelperOptions.ClientValidationEnabled = true;
             });
 
-            builder.Services.AddControllersWithViews()
-                .AddViewOptions(options =>
-                {
-                    options.HtmlHelperOptions.ClientValidationEnabled = true;
-                });
+        var app = builder.Build();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
